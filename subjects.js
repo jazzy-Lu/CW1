972c6d3e2d4c386bbc175a516a49fa0f81afbec7
Omololu Omogbehin <74530807+jazzy-Lu@users.noreply.github.com>
2020-11-20 15:57:36 +0000

    Add files via upload

---
+++ subjects.js
@@ -0,0 +1,82 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Maths",
+        location: "London",
+        price: 10000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 2,
+        title: "Physics",
+        location: "Oxford",
+        price: 12000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 3,
+        title: "English",
+        location: "London",
+        price: 11000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 4,
+        title: "Chemistry",
+        location: "York",
+        price: 12000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 5,
+        title: "Music",
+        location: "Bristol",
+        price: 9000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 6,
+        title: "Computer Science",
+        location: "London",
+        price: 10000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 7,
+        title: "French",
+        location: "Oxford",
+        price: 10000,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 8,
+        title: "Japanese",
+        location: "London",
+        price: 10500,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 9,
+        title: "Spanish",
+        location: "York",
+        price: 8500,
+        image: "images/txtig.jpg",
+        spaces: 5
+    },
+    {
+        id: 10,
+        title: "Korean",
+        location: "Bristol",
+        price: 9500,
+        image: "images/txtig.jpg",
+        spaces: 5
+    }
+]
